12b10586d8949700d5a125e924e5a89bcc5c48ee
Vbrav01 <victortosh001@gmail.com>
2024-11-16 10:39:10 +0300

    First Django project

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,26 +4,28 @@ from django.urls import reverse_lazy
 from .models import Library, Librarian, Book, Author
 
 
-
 # Create your views here.
+
 
 def list_books(request):
     books = Book.objects.all()
     response = "<h1>List of Books</h1><ul>"
-    context = 'listbooks'
+    context = "listbooks"
     for book in books:
         response += f"<li>{book.title} by {book.author.name}</li>"
     response += "</ul>"
-    return HttpResponse(response,context)
-    
+    return HttpResponse(
+        "This view should render a simple text list of book titles and their authors."
+    )
+
+
 class SpecificLibraryView(ListView):
     model = Library
-    template_name = ''
-    context_object_name = 'library'
-    success_url = reverse_lazy('home')
-    
-    def get_queryset(self,name):
+    template_name = ""
+    context_object_name = "library"
+    success_url = reverse_lazy("home")
+
+    def get_queryset(self, name):
         library = super().get_queryset()
-        specific_library = library.filter(name = name)
-        return specific_library    
-    
+        specific_library = library.filter(name=name)
+        return specific_library
