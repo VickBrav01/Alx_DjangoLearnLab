dea8908045f048faa49f8835cfedab7ede6677d6
Vbrav01 <victortosh001@gmail.com>
2024-11-24 21:34:29 +0300

    Api development

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -5,5 +5,6 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=20)
     
-    
+    def __str__(self):
+        return self.title
     
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from .serializer import BookSerializer
+from .models import Book
+from rest_framework.generics import ListAPIView
 
 # Create your views here.
+class BookList(ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+    
+    
