2be637a5b5d84fbe3d2113b3aa0db8bf309cade8
Vbrav01 <victortosh001@gmail.com>
2024-12-07 12:53:39 +0300

    Django app development

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import HomeView, RegisterView, PostView
+
+
+urlpatterns = [
+    path("home/", HomeView.as_view(), name="home"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("posts/", PostView.as_view(), name="posts"),
+]
---
+++ django_blog/blog/views.py
@@ -0,0 +1,30 @@
+# from django.shortcuts import render
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
+from rest_framework.permissions import IsAuthenticated
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Post
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class HomeView(TemplateView):
+    template_name = "blog/base.html"
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "blog/register.html"
+    success_url = reverse_lazy("home")
+
+
+class PostView(LoginRequiredMixin, ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    authentication_class = [IsAuthenticated]
+    template_name = "blog/posts.html"
